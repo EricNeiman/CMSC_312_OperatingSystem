e55c6b96a182b0de82dedf89d471f8a287ac81fa
Eric Neiman <ericneiman25@gmail.com>
2018-10-14 17:55:21 -0400

    Added jobFiles and programFiles

--- OpSim.cpp
+++ OpSim.cpp
@@ -1,26 +1,40 @@
+/*      Eric Neiman
+*       CMSC 312, Intro to Operating Systems
+*       Operating System Simulation Project Main file
+*/
+
 #include <iostream>
 #include <fstream>
 #include <string>
 using namespace std;
 
+#define maxRam = 2048; // maximum amount of ram (int Mb) available to the operating system
+
+
 void helpMenu() {
     cout << "\nList of commands";
-    cout << "\n1";
+    cout << "\nRun a program: run <program name>";
     cout << "\n2";
 }
 
 int main(int argc, char* argv[]) {
-
+    bool running = true; // is the operating system running
     string command = "";
-    while (true) {
+    while (running) {
         cout << "\nPlease enter a command: ";
         cin >> command;
-        cout << "\nExecuting " << command;
+        cout << "\nExecuting " << command << "...";
 
         if (command == "help") {
             helpMenu();
         }
-
     }
 
 }
+
+// States of a process
+// new: the process is being created
+// running: instructions are being executed
+// waiting: the process is waiting for some event to occur
+// ready: the process is waiting to be assigned to a processor
+// terminated: the process has nished execution
